4342521b0eb98d02119682bcb1e733a3c8299917
Vitaly Fanaskov <vitaly.fanaskov@zivid.com>
2022-05-31 10:21:35 +0200

    [namedtype] add new port

---
+++ ports/namedtype/portfile.cmake
@@ -0,0 +1,23 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO joboccara/NamedType
+  REF 77a95c8002a28f5cb48d2d0cd985904d66912af3
+  SHA512 1f851ccf9caf84c46bbe0b381b0d71a1e3dd06a476a94426ab178e08f546305542e4adbd8fe064a677100f3118d4b880de238888c7aeca2bf7e7882decee5bd0
+  HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+)
+vcpkg_install_cmake()
+# TODO: check whether we need it?
+# vcpkg_cmake_config_fixup()
+# vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
